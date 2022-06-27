e6a382c13c7ab062d81ab9e8e07bd503d88bee71
Krissh105 <jainkarishma76@gmail.com>
2022-06-27 21:56:54 +0530

    bug fix

--- src/components/CountryContainer.js
+++ src/components/CountryContainer.js
@@ -5,12 +5,11 @@ export const CountryContainer = ({data}) => {
         <div className="countries-container grid gap-x-[2%] justify-center gap-y-11">
             {
               data.map((el,i) => {
-                let linkPath = `/rest-countries/${el.name["common"]}/`;
                 return (
                   <div className="country-card w-full hover:scale-105 rounded-sm transition-transform duration-200 ease-in-out" key={i}>
                     { el.flags.svg && <img src={el.flags.svg} alt="" className="w-full object-cover h-[12.3rem] border-b-2 rounded-sm"/> }
                     <div className="country-card-text py-5 px-6">
-                      <Link to={linkPath}>
+                      <Link to={'/rest-countries/' + el.name["common"] + '/'}>
                         <h2 className="mb-5 cursor-pointer text-lg">{el.name["common"]}</h2>
                       </Link>
                       <div><span>Population : </span> {el.population}</div>
