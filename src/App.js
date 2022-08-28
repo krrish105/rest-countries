aad5e8832f31ebc02446e3f8331d1a9202690b37
Krrish105 <jainkarishma76@gmail.com>
2022-08-28 13:12:08 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,104 +1,133 @@
-import { useEffect, useState} from 'react';
-import './App.css';
-import { Header } from './components/Header';
-import { SearchFilter } from './components/SearchFilter';
-import { CountryContainer } from './components/CountryContainer';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Header } from "./components/Header";
+import { SearchFilter } from "./components/SearchFilter";
+import { CountryContainer } from "./components/CountryContainer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Country } from './components/Country';
-import axios from 'axios';
+import { Country } from "./components/Country";
+import axios from "axios";
+import useLocalStorage from "use-local-storage";
 
 function App() {
-  // States
-  const [region, setRegion] = useState("Filter by Region");
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [countriesData, setCountriesData] = useState([]);
-  const [url, setUrl] = useState("https://restcountries.com/v3.1/all");
+	// States
+	const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+	const [theme, setTheme] = useLocalStorage(
+		"theme",
+		defaultDark ? "dark" : "light"
+	);
+	const [region, setRegion] = useState("Filter by Region");
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [countriesData, setCountriesData] = useState([]);
+	const [url, setUrl] = useState("https://restcountries.com/v3.1/all");
 
-  // fetch handler for countries data
-  const fetchHandler = async (url) => {
-    try{
-        let res = await axios.get(url);
-        if(res.status === 404){
-            setError('Countries not found');
-            setLoading(false);
-        }else{
-            setCountriesData(res.data);
-            setLoading(false);
-            setError(false);
-        }
-    }
-    catch(error){
-        setLoading(false);
-        setError(error.message);
-    }
-  }
+	// switchTheme
+	const switchTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		document.documentElement.classList.remove(theme);
+		document.documentElement.classList.add(newTheme);
+		setTheme(newTheme);
+	};
 
-  // call the function handler once every render
-  useEffect(() => {
-    fetchHandler(url);
-     // eslint-disable-next-line
-  }, []); 
+	// fetch handler for countries data
+	const fetchHandler = async (url) => {
+		try {
+			let res = await axios.get(url);
+			if (res.status === 404) {
+				setError("Countries not found");
+				setLoading(false);
+			} else {
+				setCountriesData(res.data);
+				setLoading(false);
+				setError(false);
+			}
+		} catch (error) {
+			setLoading(false);
+			setError(error.message);
+		}
+	};
 
-  // call the function handler when the url is changed
-  useEffect(() => {
-    fetchHandler(url);
-     // eslint-disable-next-line
-  }, [url]);
-  
-  // Submit Handler
-  const submitHandler = (e) => {
-    if(e.length > 3){
-      setUrl(`https://restcountries.com/v3.1/name/${e}`);
-    }
-    if(e.length === 0){
-      setUrl("https://restcountries.com/v3.1/all");
-    }
-    setRegion('Filter by region');
-  }
+	// call the function handler once every render
+	useEffect(() => {
+		fetchHandler(url);
+		document.documentElement.classList.add(theme);
+		// eslint-disable-next-line
+	}, []);
 
-  // set Region Handler
-  const regionHandler = (r) => {
-    setRegion(r);
-    if(r && r !== 'All'){
-      setUrl(`https://restcountries.com/v3.1/region/${r}`);
-    }else{
-      setRegion("Filter by region");
-      setUrl("https://restcountries.com/v3.1/all");
-    }
-  }
+	// call the function handler when the url is changed
+	useEffect(() => {
+		fetchHandler(url);
+		// eslint-disable-next-line
+	}, [url]);
 
-  // Get Country Name
-  const getCountryName = (code) => {
-    return countriesData.filter((element) => {
-      return element.cca3 === code;
-    }).map((el, i) => el.name.common)
-  }
+	// Submit Handler
+	const submitHandler = (e) => {
+		if (e.length > 3) {
+			setUrl(`https://restcountries.com/v3.1/name/${e}`);
+		}
+		if (e.length === 0) {
+			setUrl("https://restcountries.com/v3.1/all");
+		}
+		setRegion("Filter by region");
+	};
 
-  // HomePage Component
-  const HomePage = () => {
-    return (
-      <>
-        <SearchFilter region = {region} setRegion = {regionHandler} submitHandler = {submitHandler}/>
-        { error ? <div className="font-bold text-2xl">{error}</div> : 
-          loading && !countriesData.error ? <div className="font-bold text-2xl">Loading...</div> :  <CountryContainer data={countriesData}/> }
-      </>
-    )
-  }
+	// set Region Handler
+	const regionHandler = (r) => {
+		setRegion(r);
+		if (r && r !== "All") {
+			setUrl(`https://restcountries.com/v3.1/region/${r}`);
+		} else {
+			setRegion("Filter by region");
+			setUrl("https://restcountries.com/v3.1/all");
+		}
+	};
 
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route exact path='/rest-countries/' element={ <HomePage /> }></Route>
-            <Route exact path='/rest-countries/:countryName/' element= { <Country getCountryName = {getCountryName} /> }></Route>
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+	// Get Country Name
+	const getCountryName = (code) => {
+		return countriesData
+			.filter((element) => {
+				return element.cca3 === code;
+			})
+			.map((el, i) => el.name.common);
+	};
+
+	// HomePage Component
+	const HomePage = () => {
+		return (
+			<>
+				<SearchFilter
+					region={region}
+					setRegion={regionHandler}
+					submitHandler={submitHandler}
+				/>
+				{error ? (
+					<div className="font-bold text-2xl">{error}</div>
+				) : loading && !countriesData.error ? (
+					<div className="font-bold text-2xl">Loading...</div>
+				) : (
+					<CountryContainer data={countriesData} />
+				)}
+			</>
+		);
+	};
+
+	return (
+		<Router>
+			<div className="App" data-theme={theme}>
+				<Header switchTheme={switchTheme} />
+				<main>
+					<Routes>
+						<Route exact path="/rest-countries/" element={<HomePage />}></Route>
+						<Route
+							exact
+							path="/rest-countries/:countryName/"
+							element={<Country getCountryName={getCountryName} />}
+						></Route>
+					</Routes>
+				</main>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,49 +1,78 @@
-export const SearchFilter = ({region, setRegion, submitHandler}) => {
-    return (
-        <div className="search-filter mt-8 mb-12 flex flex-col sm:flex-row gap-4">
+export const SearchFilter = ({ region, setRegion, submitHandler }) => {
+	return (
+		<div className="search-filter mt-8 mb-12 flex flex-col sm:flex-row gap-4">
+			{/* Country Search */}
+			<form
+				className="flex items-center py-4 px-6 w-full sm:w-fit"
+				onSubmit={(e) => {
+					e.preventDefault();
+					submitHandler(e.target[0].value);
+				}}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					viewBox="0 0 16 16"
+				>
+					<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+				</svg>
+				<input
+					type="text"
+					name="search"
+					id="search"
+					className="border-0 pl-5 text-base outline-none"
+					placeholder="Search for a country..."
+					autoComplete="true"
+				/>
+			</form>
 
-            {/* Country Search */}
-            <form className="flex items-center py-4 px-6 w-full sm:w-fit" onSubmit={(e) => { e.preventDefault(); submitHandler(e.target[0].value) }}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
-                <input 
-                    type="text" 
-                    name="search" 
-                    id="search" 
-                    className="border-0 pl-5 text-base outline-none" 
-                    placeholder="Search for a country..."
-                    autoComplete="true"/>
-            </form>
+			{/* Region Selector */}
+			<fieldset className="region-selector flex flex-col w-[250px] relative">
+				<div
+					className="py-3 px-4 relative flex legend cursor-pointer"
+					onClick={() =>
+						document.querySelector(".dropdown").classList.toggle("active-drop")
+					}
+				>
+					<span id="filter-text">{region}</span>
+					<div className="mt-1 absolute right-6">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							viewBox="0 0 16 16"
+						>
+							<path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+						</svg>
+					</div>
+				</div>
+				<div className="dropdown absolute w-full block">
+					<InputFields name="All" setRegion={setRegion} />
+					<InputFields name="Africa" setRegion={setRegion} />
+					<InputFields name="America" setRegion={setRegion} />
+					<InputFields name="Asia" setRegion={setRegion} />
+					<InputFields name="Europe" setRegion={setRegion} />
+					<InputFields name="Oceania" setRegion={setRegion} />
+				</div>
+			</fieldset>
+		</div>
+	);
+};
 
-            {/* Region Selector */}
-            <fieldset className="region-selector flex flex-col w-[250px] relative">
-                <div className="py-3 px-4 relative flex legend cursor-pointer" onClick={() => document.querySelector('.dropdown').classList.toggle('active-drop')}>
-                    <span id="filter-text">{region}</span>
-                    <div className="mt-1 absolute right-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-                        </svg>
-                    </div>
-                </div>
-                <div className="dropdown absolute w-full block">
-                    <InputFields name="All" setRegion = {setRegion} />
-                    <InputFields name="Africa" setRegion = {setRegion} />
-                    <InputFields name="America" setRegion = {setRegion} />
-                    <InputFields name="Asia" setRegion = {setRegion} />
-                    <InputFields name="Europe" setRegion = {setRegion} />
-                    <InputFields name="Oceania" setRegion = {setRegion} />
-                </div>
-            </fieldset>
-        </div>
-    );
-}
-
-const InputFields = ({name, setRegion}) => {
-    return(
-        <div>
-            <input type="radio" name="region" id={name}  value={name} onClick={(e) => setRegion(e.target.value)}/>
-            <label htmlFor={name}>{name}</label>
-        </div>
-    )
-}
+const InputFields = ({ name, setRegion }) => {
+	return (
+		<div>
+			<input
+				type="radio"
+				name="region"
+				id={name}
+				value={name}
+				onClick={(e) => setRegion(e.target.value)}
+			/>
+			<label htmlFor={name}>{name}</label>
+		</div>
+	);
+};
