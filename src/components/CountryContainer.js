dcb3226853c2ccf11c5256c9027addee2af786f1
Krrish105 <jainkarishma76@gmail.com>
2022-10-22 22:26:19 +0530

    fixed bug

--- src/components/CountryContainer.js
+++ src/components/CountryContainer.js
@@ -2,29 +2,23 @@ import { Link } from "react-router-dom";
 
 export const CountryContainer = ({ data }) => {
 	return (
-		<div className="countries-container grid gap-x-[2%] justify-center gap-y-11">
+		<div className='countries-container grid gap-x-[2%] justify-center gap-y-11'>
 			{data.map((el, i) => {
 				return (
 					<div
-						className="country-card w-full hover:scale-105 rounded-sm transition-transform duration-200 ease-in-out"
+						className='country-card w-full hover:scale-105 rounded-sm transition-transform duration-200 ease-in-out'
 						key={i}
 					>
 						{el.flags.svg && (
 							<img
 								src={el.flags.svg}
-								alt=""
-								className="w-full object-cover h-[12.3rem] border-b-2 rounded-sm"
+								alt=''
+								className='w-full object-cover h-[12.3rem] border-b-2 rounded-sm'
 							/>
 						)}
-						<div className="country-card-text py-5 px-6">
-							<Link
-								to={
-									"/rest-countries/" +
-									el.name["common"].replaceAll(" ", "") +
-									"/"
-								}
-							>
-								<h2 className="mb-5 cursor-pointer text-lg">
+						<div className='country-card-text py-5 px-6'>
+							<Link to={"/rest-countries/" + el.name["common"] + "/"}>
+								<h2 className='mb-5 cursor-pointer text-lg'>
 									{el.name["common"]}
 								</h2>
 							</Link>
